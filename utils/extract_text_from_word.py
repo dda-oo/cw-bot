067488e319db9123c703b280f375e90a75e27a5e
Daryoosh Dehestani <83342403+dda-oo@users.noreply.github.com>
2025-05-13 09:28:41 +0200

    Delete utils/extract_text_from_word.py

--- utils/extract_text_from_word.py
+++
@@ -1,8 +0,0 @@
-from docx import Document
-
-def extract_text_from_word(word_file):
-    doc = Document(word_file)
-    text = ""
-    for para in doc.paragraphs:
-        text += para.text + "\n"
-    return text
